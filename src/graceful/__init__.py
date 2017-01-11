4599a203c0b8429e7d946e6f03eafa5b0e1ba02a
mjaworski <mjaworski@opera.com>
2017-01-11 12:51:14 +0100

    chore: prepare new release

--- src/graceful/__init__.py
+++ src/graceful/__init__.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 """Minimalist framework for self-descriptive REST APIs build on top of falcon.
 
 It is inspired by Django REST Framework package. Mostly by how object
 serialization is done but more emphasis is put on API to be self-descriptive.
 """
-VERSION = (0, 2, 0)  # PEP 386  # noqa
+VERSION = (0, 3, 0)  # PEP 386  # noqa
 __version__ = ".".join([str(x) for x in VERSION])  # noqa
