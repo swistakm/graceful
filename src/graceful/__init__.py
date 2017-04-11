7ca1ac77c3b4adc7052f09720f752475f8be510a
mjaworski <mjaworski@opera.com>
2017-04-11 12:25:50 +0200

    dist: bump version

--- src/graceful/__init__.py
+++ src/graceful/__init__.py
@@ -3,5 +3,5 @@
 It is inspired by Django REST Framework package. Mostly by how object
 serialization is done but more emphasis is put on API to be self-descriptive.
 """
-VERSION = (0, 4, 0)  # PEP 386  # noqa
+VERSION = (0, 4, 1)  # PEP 386  # noqa
 __version__ = ".".join([str(x) for x in VERSION])  # noqa
