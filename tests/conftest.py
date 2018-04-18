e9381a6d72a711142c476f16532c8b689696c1a5
Michał Jaworski <mjaworski@vewd.com>
2018-04-18 10:34:06 +0200

    supress future warnings in tests for cleaner logs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,8 @@
-# those fixtures will be available for whole tests package
-from .fixtures import req, resp  # noqa
+import warnings
+
+# these fixtures will be available for whole tests package
+from .fixtures import *  # noqa
+
+
+# supress all future warnings for better log output
+warnings.simplefilter("ignore", FutureWarning)
